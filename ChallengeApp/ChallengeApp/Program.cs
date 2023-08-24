58e0895fbc085c3ac035a9dfa876e90e74980b10
Admin <woronczukprzemek@gmail.com>
2023-08-25 00:37:44 +0200

    zadanie domowe dzień 5

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,19 +1,65 @@
-﻿using System.ComponentModel.Design;
+﻿var number = 4594;
+var numberAsString = number.ToString();
+char[] letters = numberAsString.ToArray();
 
-var name = "Ewa";
-int age = 33;
-var gender = "Kobieta";
+var counter0 = 0;
+var counter1 = 0;
+var counter2 = 0;
+var counter3 = 0;
+var counter4 = 0;
+var counter5 = 0;
+var counter6 = 0;
+var counter7 = 0;
+var counter8 = 0;
+var counter9 = 0;
 
 
-if (gender == "Kobieta" && age < 30)
+foreach (var letter in letters)
 {
-    Console.WriteLine("Kobieta poniźej 30 lat");
+
+
+    if (letter == '0')
+    {
+        counter0++;
+    }
+    else if (letter == '1')
+    {
+        counter1++;
+    }
+    else if (letter == '2')
+    {
+        counter2++;
+    }
+    else if (letter == '3')
+    {
+        counter3++;
+    }
+    else if (letter == '4')
+    {
+        counter4++;
+    }
+    else if (letter == '5')
+    {
+        counter5++;
+    }
+    else if (letter == '6')
+    {
+        counter6++;
+    }
+    else if (letter == '7')
+    {
+        counter7++;
+    }
+    else if (letter == '8')
+    {
+        counter8++;
+    }
+    else if (letter == '8')
+    {
+        counter9++;
+    }
+
 }
-else if (name == "Ewa" && age == 30) 
-{
-    Console.WriteLine("Ewa, 30lat");
-}
-else if (gender != "Kobieta" && age < 18) 
-{
-    Console.WriteLine("Niepełnoletni męszczyzna");
-}
+
+
+
