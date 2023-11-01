8cc3e127047717b25374389fee8a4c44ee7764f4
Admin <woronczukprzemek@gmail.com>
2023-11-01 23:41:12 +0100

    zadanie domowe dzień 4

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,44 +1,20 @@
-﻿
+﻿var name = "Ewa";
+var gender = "Kobieta";
+var age = 30;
 
-using ChallengeApp;
-
-Employee user1 = new Employee("Adam", "Kowalski", 24);
-Employee user2 = new Employee("Monika", "Mak", 30);
-Employee user3 = new Employee("Błażej", "Jeziorski",18);
-
-user1.AddScore(5);                             
-user1.AddScore(2);
-user1.AddScore(1);
-user1.AddScore(3);
-user1.AddScore(6);
-
-user2.AddScore(2);
-user2.AddScore(5);
-user2.AddScore(9);
-user2.AddScore(9);
-user2.AddScore(2);                       
-
-user3.AddScore(6);
-user3.AddScore(1);
-user3.AddScore(1);
-user3.AddScore(2);
-user3.AddScore(5);
-
-
-List<Employee> users = new List<Employee>()
+if (gender == "Kobieta" && age < 33)
 {
-    user1, user2, user3
-};
-
-int maxResult = -1;
-Employee userWithMaxResult = null;
-
-foreach (var user in users)
-{
-    if (user.Result > maxResult)
-    {
-        userWithMaxResult = user;
-    }
+    Console.WriteLine("Kobieta poniżej 30lat");
 }
-
-Console.WriteLine("Osoba z najwyższą punktacją" + userWithMaxResult.Name + "" + userWithMaxResult.Surename + "wiek:" + userWithMaxResult.Age + "punkty:" + userWithMaxResult.Result);
+else if (name == "Ewa" && age == 33)
+{
+    Console.WriteLine("Ewa lat 33");
+}
+else if (gender == "Kobieta" && age > 33)
+{
+    Console.WriteLine("Kobieta powyżej 30lat");
+}
+else if (gender == "Męszczyzna" && age < 18)
+{
+    Console.WriteLine("Niepełnoletni Męszczyzna");
+}
