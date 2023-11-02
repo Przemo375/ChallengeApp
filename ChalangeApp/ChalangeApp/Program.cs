aa1f844888f6448f16ab027395fa46d8fb367310
Admin <woronczukprzemek@gmail.com>
2023-11-02 12:09:44 +0100

    zadanie domowe dzień 4

---
+++ ChalangeApp/ChalangeApp/Program.cs
@@ -0,0 +1,21 @@
+﻿var name = "Ewa";
+var gender = "Kobieta";
+var age = 40;
+
+
+ if (name == "Ewa" && age == 33)
+{
+    Console.WriteLine("Ewa lat 33");
+}
+else if (gender == "Kobieta" && age < 30)
+{
+    Console.WriteLine("Kobieta poniżej 30lat");
+}
+else if (gender == "Kobieta" && age > 30)
+{
+    Console.WriteLine("Kobieta powyżej 30lat");
+}
+else if (gender == "Męszczyzna" && age < 18)
+{
+    Console.WriteLine("Niepełnoletni Męszczyzna");
+} 
